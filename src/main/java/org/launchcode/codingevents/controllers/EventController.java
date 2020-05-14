249a231c8f5220d1c5a81b8f179f1eecd7db69a2
Milani <PMilan000@cable.comcast.com>
2020-05-14 11:55:56 -0400

    update

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,9 +2,7 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,18 +11,22 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
         public String displayEvents(Model model){
-        List events = new ArrayList<>();
-        events.add("Tech Crunch");
-        events.add("Hack the Planet");
-        events.add("Activate Skynet");
-        model.addAttribute("events", events);
-        return "events/index";
+            model.addAttribute("events", events);
+            return "events/index";
 
     }
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
     }
 }
